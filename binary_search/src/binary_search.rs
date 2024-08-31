eb2274435301cc98610042ed537d46a58c86d19d
JorgeV92 <jorgev2@illinois.edu>
2024-08-30 23:28:08 -0500

    rust binary

--- binary_search/src/binary_search.rs
+++ binary_search/src/binary_search.rs
@@ -16,3 +16,46 @@ pub fn binary_search(a: &mut [i32], t :i32) -> i32 {
     -1
 }
 
+
+// pub fn last_true(mut lo :i32, mut hi :i32, f :fn(i32) -> bool) -> i32 {
+//     lo -= 1;
+//     while lo < hi {
+//         let mid :i32 = lo + (hi - lo + 1) / 2;
+//         if f(mid) {
+//             lo = mid;
+//         } else {
+//             hi = mid - 1;
+//         }
+//     }
+//     lo
+// }
+
+
+
+pub fn last_true<F>(mut lo :i64, mut hi :i64, f :F) -> i64
+where 
+    F: Fn(i64) -> bool,
+{
+    lo -= 1;
+    while lo < hi {
+        let mid = lo + (hi - lo + 1) / 2;
+        if f(mid) {
+            lo = mid;
+        } else {
+            hi = mid - 1;
+        }
+    }
+    lo
+}
+
+pub fn always_true(_ :i32) -> bool {
+    true
+}
+
+pub fn always_false(_ :i32) -> bool {
+    false
+}
+
+pub fn check(x :i32) -> bool {
+    x * x <= 30
+}
--- binary_search/src/main.rs
+++ binary_search/src/main.rs
@@ -1,38 +1,47 @@
 
+use std::io;
 mod binary_search;
 
-fn last_true(mut lo :i32, mut hi :i32, f :fn(i32) -> bool) -> i32 {
-    lo -= 1;
-    while lo < hi {
-        let mid :i32 = lo + (hi - lo + 1) / 2;
-        if f(mid) {
-            lo = mid;
-        } else {
-            hi = mid - 1;
-        }
-    }
-    lo
-}
-
-fn always_true(_ :i32) -> bool {
-    true
-}
-
-fn always_false(_ :i32) -> bool {
-    false
-}
-
-fn check(x :i32) -> bool {
-    x * x <= 30
-}
 
 fn main() {
-    println!("{}", last_true(2, 10, always_true));
-    println!("{}", last_true(2, 10, check));
-    println!("{}", last_true(2, 10, always_false));
+    // println!("{}", binary_search::last_true(2, 10, binary_search::always_true));
+    // println!("{}", binary_search::last_true(2, 10, binary_search::check));
+    // println!("{}", binary_search::last_true(2, 10, binary_search::always_false));
 
     let mut arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     let target = 5;
     let result = binary_search::binary_search(&mut arr, target);
     println!("Element found at index: {}", result);
+
+    // Maximum Medium problem
+    // https://codeforces.com/contest/1201/problem/C
+    let mut first = String::new();
+    io::stdin().read_line(&mut first).expect("Failed to read line");
+
+    let mut iter = first.split_whitespace();
+    let n: usize = iter.next().unwrap().parse().expect("Failed to parse n");
+    let k: i64 = iter.next().unwrap().parse().expect("Failed to parse k");
+
+    let mut second = String::new();
+    io::stdin().read_line(&mut second).expect("Failed to read line");
+
+    let mut vec: Vec<i32> = second
+        .split_whitespace()
+        .map(|s| s.parse().expect("Failed to parse number"))
+        .collect();
+
+    println!("n = {}, k = {}", n, k);
+    println!("Vector: {:?}", vec);
+
+    vec.sort();
+    println!("Vector: {:?}", vec);
+
+    let res = binary_search::last_true(1, 2_000_000_000, |x| {
+        let mut ops_needed: i64 = 0;
+        for i in (n - 1) / 2..n {
+            ops_needed += (x - vec[i] as i64).max(0);
+        }
+        ops_needed <= k
+    });
+    println!("{}",  res);
 }
